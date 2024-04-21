80619088d32986de0c445e56d6e85422b22bd915
lavneesh583 <lavneesh.garg@sehajsolution.com>
2024-04-21 22:54:38 +0530

    project upload

---
+++ lib/actions/category.actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { CreateCategoryParams } from "@/types";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
+
+export const createCategory = async ({
+  categoryName,
+}: CreateCategoryParams) => {
+  try {
+    await connectToDatabase();
+    const newCategory = await Category.create({ name: categoryName });
+    return JSON.parse(JSON.stringify(newCategory));
+  } catch (e) {
+    handleError(e);
+  }
+};
+
+export const getAllCategories = async () => {
+  try {
+    await connectToDatabase();
+    const categories = await Category.find();
+    return JSON.parse(JSON.stringify(categories));
+  } catch (e) {
+    handleError(e);
+  }
+};
